53ecd30a7b770cb57c0bb1b9b5469cac589612de
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-10 16:11:53 +0300

    scope bean

--- src/main/java/com/example/demo/rest/MyController.java
+++ src/main/java/com/example/demo/rest/MyController.java
@@ -10,14 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     private Teacher teacher;
+    private Teacher secondTeacher;
 
     @Autowired
-    public MyController(@Qualifier("mathTeacher") Teacher teacher) {
+    public MyController(@Qualifier("mathTeacher") Teacher teacher,
+                        @Qualifier("mathTeacher") Teacher secondTeacher) {
         System.out.println("Bean of class - " + this.getClass().getName());
+        this.secondTeacher = secondTeacher;
         this.teacher = teacher;
     }
     @GetMapping("/homework")
     public String getHomeWork() {
         return teacher.getHomeWork();
+    }
+
+    @GetMapping("/compTeachers")
+    public String compareTeachers() {
+        return "First teacher - " + teacher + "....." +
+                "Second teacher - " + secondTeacher + "....." +
+                "CompareTeachers - " + (teacher == secondTeacher);
     }
 }

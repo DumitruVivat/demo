c47aa6f63b359e8a228f3b219fae88c44881a6ed
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-10 13:56:25 +0300

    add teacher class

---
+++ src/main/java/com/example/demo/MathTeacher.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MathTeacher implements Teacher{
+    @Override
+    public String getHomeWork() {
+        return "Solve equation";
+    }
+}
